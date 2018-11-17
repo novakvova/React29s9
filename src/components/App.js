981eaa12cacf7c283f921585cc69dd6b06488f26
novakvova <novakvova@gmail.com>
2018-11-17 13:18:26 +0200

    Add btn Reverse button

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ArticleList from './ArticleList/index';
 import listArticles from '../data';
 import 'bootstrap/dist/css/bootstrap.min.css';
-function App() {
-    return (
-        <div className="container">
-            <div className="jumbotron">
-                <h1 className="display-3">Hello react!</h1>
-            </div>
-            <ArticleList articles={listArticles} />
-        </div>
-    )
-}
+class App extends Component {
 
+    state = {
+        reverted: false
+    }
+    render() {
+        return (
+            <div className="container">
+                <div className="jumbotron">
+                    <h1 className="display-3">
+                        Hello react!
+                    <button className="btn" onClick={this.revert}>Revert</button>
+                    </h1>
+                </div>
+                <ArticleList articles={this.state.reverted ? listArticles.reverse() : listArticles} />
+            </div>
+        )
+
+    }
+    revert = () => {
+        console.log("----", "Revert click")
+        this.setState({
+            reverted: !this.state.reverted
+        });
+    }
+}
 export default App;
